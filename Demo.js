01ea0faef136a86ace8537d02b1fa73bb88d8b90
Thinh <nthinh92@gmail.com>
2020-05-21 13:59:19 +0700

    LT JavaScript

--- Demo.js
+++ Demo.js
@@ -1,15 +1,32 @@
+// let count = 0;
+// let i = 2;
+// let n = 2;
+// while (count <= 20) {
+//   while (i <= Math.sqrt(n)) {
+//     if (n % i == 0) {
+//       i++;
+//     } else {
+//       document.write("n + '<br>");
+//       n++;
+//       count++;
+//     }
+//   }
+// }
+// document.getElementById('Results').innerHTML = ("20 so nguyen to dau tien la: ");
+// let a = 1;
+// let b = 0;
+// let c;
+// while (1) {
+//   c = a + b;
+//   a = b;
+//   b = c;
+//   if (c % 5 == 0) {
+//     console.log(c);
+//     break;
+//   }
+// }
 let count = 0;
-let i = 2;
-let n = 2;
-while (count <= 20) {
-  while (i <= Math.sqrt(n)) {
-    if (n % i == 0) {
-      i++;
-    } else {
-      document.write("n + '<br>");
-      n++;
-      count++;
-    }
-  }
+while (++count <= 10) {
+  count++;
+  console.log(count);
 }
-document.getElementById('Results').innerHTML = ("20 so nguyen to dau tien la: ");
